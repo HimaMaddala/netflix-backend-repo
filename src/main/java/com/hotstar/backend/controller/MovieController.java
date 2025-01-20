f3b82d3e90d05f22dc3106a1b78886a7bf613cd2
Hima Shankar <121500229+HimaMaddala@users.noreply.github.com>
2025-01-20 10:34:11 +0530

    Update MovieController.java

--- src/main/java/com/hotstar/backend/controller/MovieController.java
+++ src/main/java/com/hotstar/backend/controller/MovieController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 public class MovieController {
     @GetMapping("/health")
-    public List<Movie> healtCheck {
+    public String healtCheck (){
         return "Test Successfull";
     }
 
